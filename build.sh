2866b0fec34c78a46d9513a05bf70f0db14fa436
fritzprix <innocentevil0914@gmail.com>
2022-05-07 15:16:11 +0900

    removed

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build -t pytorch_lab:0.1.0 .
-mkdir data tf_logs work > /dev/null 2>&1
--- start_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-jt -t grade3
-xvfb-run -s "-screen 0 1400x900x24" jupyter notebook --ip=0.0.0.0 --port 8888 --allow-root
