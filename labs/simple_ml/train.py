fc81c0df1c8c0da0272faeab3b009f172fc90087
fritzprix <innocentevil0914@gmail.com>
2023-01-21 10:33:15 +0900

    update

--- labs/simple_ml/train.py
+++ labs/simple_ml/train.py
@@ -13,13 +13,13 @@ import os
 def main(args: Namespace):
     
     wandb_logger = loggers.WandbLogger(project='simple_glm_character')
-    train_dataset = WikiDataset(n_steps=40, target='train')
-    val_dataset = WikiDataset(n_steps=40, target='validation')
+    train_dataset = WikiDataset(n_steps=64, target='train')
+    val_dataset = WikiDataset(n_steps=64, target='validation')
     collate_fn = train_dataset.collater()
     train_dataloader = data.DataLoader(dataset=train_dataset, batch_size=args.batch, num_workers=os.cpu_count(), collate_fn=collate_fn)
     val_dataloader = data.DataLoader(dataset=val_dataset, batch_size=args.batch, num_workers=os.cpu_count(), collate_fn=collate_fn)
-    
-    model = GRUML(input_size=len(train_dataset.vocab), lr=args.lr, padding_id=train_dataset.vocab['<pad>'])
+    input_size = len(train_dataset.vocab)
+    model = GRUML(input_size=input_size, lr=args.lr, num_hidden=input_size, padding_id=train_dataset.vocab['<pad>'])
     
     ckpt_callback = callbacks.ModelCheckpoint('./model/gru_ml/', 
                                               filename='model-{epoch}-{val_loss:.3f}', 
@@ -41,6 +41,6 @@ def main(args: Namespace):
 if __name__ == '__main__':
     arg_parser = ArgumentParser('train.py')
     arg_parser.add_argument('--batch', type=int, default=40)
-    arg_parser.add_argument('--lr', type=float, default=1e-4)
+    arg_parser.add_argument('--lr', type=float, default=1e-5)
     arg_parser.add_argument('--max_epoch', type=int, default=100)
     main(arg_parser.parse_args())
