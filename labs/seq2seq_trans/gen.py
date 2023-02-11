c1f80c981d9455522151938425c436e351395cad
fritzprix <innocentevil0914@gmail.com>
2023-02-11 17:46:46 +0900

    update

--- labs/seq2seq_trans/gen.py
+++ labs/seq2seq_trans/gen.py
@@ -1,13 +1,14 @@
 import argparse
 import model
+import data
 import torch
 
 LANG_CHOICE = set(["ko","en"])
 
 def main(args):
     assert args.lang in LANG_CHOICE
-    tokenizer = model.get_default_tokenizer(args.lang)
-    encoder = model.GRUEncoder.load_from_checkpoint(f'model/encoder/{args.lang}/model.ckpt')
+    tokenizer = data.get_default_tokenizer(args.lang)
+    encoder = model.GRUModule.load_from_checkpoint(f'model/{args.lang}/model.ckpt')
     cuda = torch.device('cuda')
     tokens = tokenizer.encode(args.prompt, return_tensors="pt").to(cuda)
     encoder = encoder.cuda()
@@ -15,7 +16,7 @@ def main(args):
         
 
 if __name__ == '__main__':
-    arg_parser = argparse.ArgumentParser('predict.py')
+    arg_parser = argparse.ArgumentParser('gen.py')
     arg_parser.add_argument("--lang", choices=["ko", "en"], default="ko")
     arg_parser.add_argument("--prompt", type=str, default='이것은 다시 말해')
     arg_parser.add_argument("--len", type=int, default=20)
